ca2ba33a4decf82e22bd86c68d9b3829e1b42af2
lacontreras <107009919+lacontreras@users.noreply.github.com>
2023-01-11 14:05:13 -0600

    Add files via upload

---
+++ src/Grudge.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { GrudgeContext } from './GrudgeContext';
+
+const Grudge = React.memo(({ grudge }) => {
+  const { toggleForgiveness } = React.useContext(GrudgeContext);
+  const forgive = () => toggleForgiveness(grudge.id);
+
+  console.log('Rendering Grudge');
+
+  return (
+    <article className="Grudge">
+      <h3>{grudge.person}</h3>
+      <p>{grudge.reason}</p>
+      <div className="Grudge-controls">
+        <label className="Grudge-forgiven">
+          <input type="checkbox" checked={grudge.forgiven} onChange={forgive} />{' '}
+          Forgiven
+        </label>
+      </div>
+    </article>
+  );
+});
+
+export default Grudge;
